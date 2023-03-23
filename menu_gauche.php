5faccee87546040b9b4e22c7ed7182edf7dcc34c
evairson <eva.herson@me.com>
2023-03-23 11:28:20 +0100

    publier

--- menu_gauche.php
+++ menu_gauche.php
@@ -6,7 +6,7 @@ function menu_gauche($i){
 
     echo '<div id="menu_gauche">
     <img id="logo" src="image/logo2.png">
-    <a href="" class="menu_gauche_bouton" >
+    <a href="accueil_publi.php" class="menu_gauche_bouton" >
         <img src="image/maison_'.$icone[0].'.png">
         <p class="'.$icone[0].'"> ACCUEIL </p>
     </a>    
@@ -14,7 +14,7 @@ function menu_gauche($i){
         <img src="image/recherche_'.$icone[1].'.png">
         <p class="'.$icone[1].'"> RECHERCHE </p>
         </a> 
-    <a href="" class="menu_gauche_bouton" >
+    <a href="publier.php" class="menu_gauche_bouton" >
         <img src="image/plus_'.$icone[2].'.png">
         <p class="'.$icone[2].'"> CREER </p>
         </a> 
---
+++ publier.php
@@ -0,0 +1,69 @@
+<?php
+session_start();
+
+include("publi.php");
+$connexion = data();
+$req1= 'SELECT * FROM utilisateur WHERE id="'.$_SESSION['utilisateur'].'"';
+$resultat = mysqli_query($connexion, $req1);
+$utilisateur=mysqli_fetch_assoc($resultat);
+
+include("menu_gauche.php");
+
+if( !empty($_FILES['publication'])){
+    echo 'file';
+}
+
+if(!empty($_POST)){
+  if( !empty($_FILES['publication']['name']) ){
+            $nomImage = 'image'.$_SESSION['utilisateur'].'-'.$utilisateur['nbpubli'].'.png';
+            if(move_uploaded_file($_FILES['publication']['tmp_name'], 'publication/'.$nomImage))
+            {
+            $new_nbpubli = $utilisateur['nbpubli'] +1;
+            $req2= 'UPDATE utilisateur SET nbpubli="'.$new_nbpubli.'" WHERE id="'.$_SESSION['utilisateur'].'"';
+            mysqli_query($connexion, $req2);
+            $req3= 'INSERT INTO publication (texte,utilisateur,numero) VALUES ("'.$_POST['texte'].'","'.$_SESSION['utilisateur'].'","'.$utilisateur['nbpubli'].'")';
+            mysqli_query($connexion, $req3);
+            header('location:accueil_publi.php');
+            }
+          else
+          {
+            $message = 'Une erreur interne a empêché l\'uplaod de l\'image';
+          }
+      }
+      else
+      {
+        $message = 'Veuillez mettre une image';
+      }
+}
+
+   ?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>CampusParis</title>
+        <link rel="stylesheet" href="projet.css">
+        <meta charset="UTF-8">
+        <meta name="author" content="Kylian, Eva">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://fonts.googleapis.com/css2?family=Open+Sans" rel="stylesheet">
+
+    </head>
+    <body class="display">
+        <?php menu_gauche(2);?>
+<div class="publier_box">
+<img src="image/image.png">
+<p>Importer une photo</p>
+<form enctype="multipart/form-data" action="publier.php" method="post">
+    <?php echo $message;?>
+    <div class="importer">   
+    <label id="importer" for="file">IMPORTER</label>
+    <input type="file" name="publication" id="file"></div>
+    <input type="text" name="texte" placeholder="Legende">
+    <input type="submit" value="PUBLIER">
+</form>
+</div>
+
+        
+    </body>
+</html>
